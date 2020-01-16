0a7cad6059b79e7d9f29c3615d092f58a2f03626
Lily Han <lily.han@fcbsixconsulting.com>
2020-01-16 13:56:58 -0500

    add hero img

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,23 +1,12 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import { BLOCKS } from "@contentful/rich-text-types"
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
 
 import styles from './hero.module.css'
 
-
-
-
-export default ({ data }) => {
+export default ({ copy, img }) => {
   return (
-    <div className={styles.hero}>
-      {documentToReactComponents(data)}
+    <div className={styles.hero} style={{ backgroundImage: `url(${img})` }}>
+      {documentToReactComponents(copy)}
     </div>
   );
-
-  }
+}
