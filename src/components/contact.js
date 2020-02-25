321dcb3c2658eea9f5951f4ec5f9281432192472
Lily Han <lily.han@fcbsixconsulting.com>
2020-02-24 20:40:29 -0500

    mobile layout and add form functionality

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,68 +4,78 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import styles from './styles/contact.module.scss'
 
-export default ({ data }) => {
-  return (
-    <div>
-      <div id="contact" className={`section-deco container ${styles.contact}`}>
-        <div>
-          <h1 class="headline">{data.headline}</h1>
+export default class contact extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  submitForm() {
+    console.log(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="contact" className={`section-deco container ${styles.contact}`}>
           <div>
-            <img src={data.contactMap.file.url} alt="" />
-          </div>
-
-          <form className={styles.contactForm}>
-            <div className={styles.formRow}>
-              <label for="validationDefault01">Name:</label>
-              <input type="text" className="form-control" id="validationDefault01" required></input>
+            <h1 class="headline">{this.props.data.headline}</h1>
+            <div>
+              <img src={this.props.data.contactMap.file.url} alt="" />
             </div>
-            <div className={styles.formRow}>
-              <label for="validationDefault02">Company:</label>
-              <input type="text" className="form-control" id="validationDefault02" required></input>
-            </div>
-            <div className={styles.formRow}>
-              <label for="validationDefault03">Phone:</label>
-              <input type="number" className="form-control" id="validationDefault03" required></input>
-            </div>
-            <div className={styles.formRow}>
-              <div className={styles.formMessege}>
-                <label for="validationDefault04">Message:</label>
-                <textarea rows="1" className="form-control" id="validationDefault04" required></textarea>
+              
+            <form className={styles.contactForm} action="mailto:varun@kisaandietech.com" method="post" enctype="text/plain">
+              <div className={styles.formRow}>
+                <label for="validationDefault01">Name:</label>
+                <input type="text" name="Name" className="form-control" id="validationDefault01" required></input>
               </div>
-
-              <button className={`btn btn-primary ${styles.submitBtn}`} type="submit">Submit</button>
+              <div className={styles.formRow}>
+                <label for="validationDefault02">Company:</label>
+                <input type="text" name="Company" className="form-control" id="validationDefault02" required></input>
+              </div>
+              <div className={styles.formRow}>
+                <label for="validationDefault03">Phone:</label>
+                <input type="number"name="Phone" className="form-control" id="validationDefault03" required></input>
+              </div>
+              <div className={styles.formRow}>
+                <div className={styles.formMessege}>
+                  <label for="validationDefault04">Message:</label>
+                  <textarea rows="1" name="Message" className="form-control" id="validationDefault04" required></textarea>
+                </div>
+  
+                <button className={`btn btn-primary ${styles.submitBtn}`} type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className={styles.contacts}>
+          <div className="container">
+            <div className={styles.contactItem}>
+              <p className={styles.name}>{this.props.data.contact1Name}</p>
+              <p className={styles.title}>{this.props.data.contact1Title}</p>
+              <hr />
+              <p>{this.props.data.contact1Email}</p>
+              <p>{this.props.data.contact1PhoneNumber}</p>
             </div>
-          </form>
-        </div>
-      </div>
-      <div className={styles.contacts}>
-        <div className="container">
-          <div className={styles.contact}>
-            <p className={styles.name}>{data.contact1Name}</p>
-            <p className={styles.title}>{data.contact1Title}</p>
-            <hr />
-            <p>{data.contact1Email}</p>
-            <p>{data.contact1PhoneNumber}</p>
-          </div>
-
-          <div className={styles.contact}>
-            <p className={styles.name}>{data.contact2Name}</p>
-            <p className={styles.title}>{data.contact2Title}</p>
-            <hr />
-            <p>{data.contact2Email}</p>
-            <p>{data.contact2PhoneNumber}</p>
-          </div>
-
-          <div className={styles.contact}>
-            <p>{data.address}</p>
-            <hr />
-            <p className={styles.website}>{data.website}</p>
+  
+            <div className={styles.contactItem}>
+              <p className={styles.name}>{this.props.data.contact2Name}</p>
+              <p className={styles.title}>{this.props.data.contact2Title}</p>
+              <hr />
+              <p>{this.props.data.contact2Email}</p>
+              <p>{this.props.data.contact2PhoneNumber}</p>
+            </div>
+  
+            <div className={styles.contactItem}>
+              <p>{this.props.data.address}</p>
+              <hr />
+              <p className={styles.website}>{this.props.data.website}</p>
+            </div>
           </div>
         </div>
+        <div className={styles.footer}>
+          <p>{this.props.data.copyRight}</p>
+        </div>
       </div>
-      <div className={styles.footer}>
-        <p>{data.copyRight}</p>
-      </div>
-    </div>
-  );
+    )
+  }
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,29 +4,67 @@ import styles from './styles/navigation.module.scss'
 import logoImg from '../images/logo.svg';
 import instagram from '../images/instagram.png';
 
-export default () => (
-  <div className={`container ${styles.navigation}`}>
-    <nav role="navigation">
-      <img className={styles.navigationItem, styles.logo} src={logoImg} alt="Pilot Precision Logo" />
-      <ul className={styles.navigationItems}>
-        <li>
-          <Link to="#about">About</Link>
-        </li>
-        <li>
-          <Link to="#equipment">Equipment and Capabilities</Link>
-        </li>
-        <li>
-          <Link to="#applications">Applications</Link>
-        </li>
-        <li>
-          <Link to="#contact">Contact</Link>
-        </li>
-        <li className={`${styles.iconInsta}`}>
-          <Link to="/blog/">
-            <img src={instagram} alt="Instagram icon" width="22" height="22" />
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-)
+export default class navigation extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+        nav: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  resize() {
+    if (window.innerWidth <= 992) {
+        this.setState({nav: false});
+        console.log(this.state.nav);
+    } else {
+      this.setState({nav: true});
+      console.log(this.state.nav);
+    }
+  }
+  
+  toggleNav() {
+    this.setState((currentState) => ({
+      nav: !currentState.nav, 
+    }));
+  }
+  
+  render() {
+    return (
+      <div className={`container ${styles.navigation}`}>
+        <nav role="navigation">
+          <img className={styles.navigationItem, styles.logo} src={logoImg} alt="Pilot Precision Logo" />
+          <button className={styles.navToggle} onClick={() => this.toggleNav()}>Menu</button>
+          
+          {
+            this.state.nav?
+              <ul className={styles.navigationItems}>
+                <li>
+                  <Link to="#about">About</Link>
+                </li>
+                <li>
+                  <Link to="#equipment">Equipment and Capabilities</Link>
+                </li>
+                <li>
+                  <Link to="#applications">Applications</Link>
+                </li>
+                <li>
+                  <Link to="#contact">Contact</Link>
+                </li>
+                <li className={`${styles.iconInsta}`}>
+                  <Link to="/blog/">
+                    <img src={instagram} alt="Instagram icon" width="22" height="22" />
+                  </Link>
+                </li>
+              </ul>
+            :null
+          }
+        </nav>
+      </div>
+    )
+  }
+}
